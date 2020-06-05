382cb98427716b89fdef377e6f0b40b6cb470e0c
ksampang <48513468+ksampang@users.noreply.github.com>
2020-06-05 17:00:49 +0530

    Update Shell.sh

--- Shell.sh
+++ Shell.sh
@@ -1,1 +1,3 @@
 echo " I was able to execute this shell Script"
+/Users/karthiksampangi/Jmeter/apache-jmeter-5.1.1/bin/jmeter -n -t /Users/karthiksampangi/.jenkins/workspace/jmeter_script_execute/CP_git_Jenkins.jmx -l /Users/karthiksampangi/.jenkins/workspace/jmeter_script_execute/cp_jenkins.jtl
+echo" I am running the jmeeter test now"
