68c10f9c65a554961a6e182c6bf061138c0ebbfa
ksampang <48513468+ksampang@users.noreply.github.com>
2020-06-30 14:45:42 +0530

    Update Shell.sh

--- Shell.sh
+++ Shell.sh
@@ -3,4 +3,7 @@
 path=$(<Script_Path.txt)
 echo > Script_Name_tmp.txt
 echo $path | rev | cut -d'/' -f 1 | rev>>Script_Name_tmp.txt;cat Script_Name_tmp.txt
+v=$(cat Script_Name_tmp.txt)
+v2=${v::-4}
+echo "$v --> $v2"
 
