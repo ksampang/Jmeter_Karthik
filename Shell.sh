df13a2a55506af564f4ad27a05599abf7de6498d
ksampang <48513468+ksampang@users.noreply.github.com>
2020-06-30 15:06:35 +0530

    Update Shell.sh

--- Shell.sh
+++ Shell.sh
@@ -3,16 +3,22 @@
 path=$(<Script_Path.txt)
 echo > Script_Name_tmp.txt
 echo $path | rev | cut -d'/' -f 1 | rev>>Script_Name_tmp.txt;cat Script_Name_tmp.txt
-if [[ 'grep ".zip" $Script_Name_tmp.txt' ]]
-  then
-     v=$(cat Script_Name_tmp.txt)
-     v2=${v::-4}
-     echo $v2>>Script_Name_final.txt
-     echo "It was a zip file "
-  else 
-     cp  Script_Name_tmp.txt Script_Name_final.txt
-     echo "It was a jmx file"
+if grep -q .zip "$Script_Name_tmp.txt";
+then
+echo "Hooray!!It's available"
+else
+echo "Oops!!Not available"
 fi
+# if [[ 'grep ".zip" $Script_Name_tmp.txt' ]]
+#   then
+#      v=$(cat Script_Name_tmp.txt)
+#      v2=${v::-4}
+#      echo $v2>>Script_Name_final.txt
+#      echo "It was a zip file "
+#   else 
+#      cp  Script_Name_tmp.txt Script_Name_final.txt
+#      echo "It was a jmx file"
+# fi
 
 
 cat Script_Name_final.txt
