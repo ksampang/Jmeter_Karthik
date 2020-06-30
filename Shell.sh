b69886caad964cd14c0def111ea8361287e1270f
ksampang <48513468+ksampang@users.noreply.github.com>
2020-06-30 14:48:56 +0530

    Update Shell.sh

--- Shell.sh
+++ Shell.sh
@@ -2,8 +2,7 @@
 
 path=$(<Script_Path.txt)
 echo > Script_Name_tmp.txt
-echo $path | rev | cut -d'/' -f 1 | rev>>Script_Name_tmp.txt;cat Script_Name_tmp.txt
+echo $path | rev | cut -d'/' -f 1 | rev>>Script_Name_jmx.txt;cat Script_Name_jmx.txt
 v=$(cat Script_Name_tmp.txt)
 v2=${v::-4}
-echo "$v --> $v2"
-
+echo $v2>>Script_Name_zip.txt
